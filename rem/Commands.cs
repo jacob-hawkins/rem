5a20d7db7cdfe5082116eeb4e6fa04d9ceba9a60
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-01-25 13:47:58 -0500

    created reminder class

--- rem/Commands.cs
+++ rem/Commands.cs
@@ -1,8 +1,23 @@
 using Npgsql;
 using rem;
 using helper;
+using System.Reflection;
+using Npgsql.Replication;
 
 namespace commands {
+    public class Reminder {
+        public string title { get; set; } = string.Empty;
+        public DateTime date { get; set; }
+        public bool completed { get; set; }
+
+        public Reminder (string title, DateTime date, bool completed) {
+            this.title = title;
+            this.date = date;
+            this.completed = completed;
+        }
+
+    }
+
     public class Commands {
         public static void Init() {
             
@@ -18,7 +33,7 @@ namespace commands {
 
                 DateTime dt = DateTime.Now;
 
-                // Insert some data
+                // Insert data
                 cmd.CommandText = $"INSERT INTO reminders (user_id, title, date) VALUES (@user_id, @title, @date)";
                 cmd.Parameters.Add("@user_id", NpgsqlTypes.NpgsqlDbType.Integer).Value = 1;
                 cmd.Parameters.Add("@title", NpgsqlTypes.NpgsqlDbType.Varchar).Value = "Reminder from program";
@@ -33,7 +48,8 @@ namespace commands {
 
         public static async void View() {
             const string format = "{0} {1,-32} {2} {3, 6}";
-            int i = 1;
+            int j = 1;
+            List<Reminder> reminders = new List<Reminder>();
 
             try {
                 var con = new NpgsqlConnection(
@@ -46,19 +62,29 @@ namespace commands {
                 NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
                 
                 while (await reader.ReadAsync()) {
+                    var r = new Reminder(
+                        (string)reader[2] ?? String.Empty,
+                        (DateTime)reader[3],
+                        (bool)reader[4]
+                    );
+
+                    reminders.Add(r);
+                }
+                    
+                for (int i = 0; i < reminders.Count; i++) { 
                     string completed = "[ ]";
 
-                    if (reader[4].ToString() == "True") {
+                    if (reminders[i].completed == true) {
                         completed = "[X]";
                         
                         Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.WriteLine(format, i, reader[2], reader[3], completed);
+                        Console.WriteLine(format, j, reminders[i].title, reminders[i].date, completed);
                         Console.ResetColor();
                     } else {
-                        Console.WriteLine(format, i, reader[2], reader[3], completed);
+                        Console.WriteLine(format, j, reminders[i].title, reminders[i].date, completed);
                     }
                     
-                    i++;
+                    j++;
                 }
             } catch (Exception e) {
                 C.Error(e.Message);
