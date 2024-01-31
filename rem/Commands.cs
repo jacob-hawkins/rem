666beb4c9c33c8bc0b63ec7c97778f16d5e7d7b3
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-01-31 14:26:48 -0500

    semantic date adding

--- rem/Commands.cs
+++ rem/Commands.cs
@@ -1,7 +1,7 @@
 using rem;
 using helper;
-using view;
 using Npgsql;
+using add;
 
 namespace commands {
     public class Reminder {
@@ -24,29 +24,59 @@ namespace commands {
             
         }
 
-        public static async void Add() {
-            try {
-                var con = new NpgsqlConnection(
-                connectionString: Program.ConnectionString);
-                con.Open();
-                using var cmd = new NpgsqlCommand();
-                cmd.Connection = con;
+        public static void Add() {
+            DateTime dt;
+            string title = "";
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Remeber: ");
+            Console.ResetColor();
 
-                DateTime dt = DateTime.Now;
+            string? reminder = Console.ReadLine();
+            if (reminder == null) return;
 
-                // Insert data
-                cmd.CommandText = $"INSERT INTO reminders (user_id, title, date) VALUES (@user_id, @title, @date)";
-                cmd.Parameters.Add("@user_id", NpgsqlTypes.NpgsqlDbType.Integer).Value = 1;
-                cmd.Parameters.Add("@title", NpgsqlTypes.NpgsqlDbType.Varchar).Value = "Reminder from program";
-                cmd.Parameters.Add("@date", NpgsqlTypes.NpgsqlDbType.Date).Value = dt;
-                await cmd.ExecuteNonQueryAsync();
 
-                con.Close();
+            if (reminder.Contains("on ")) {
+                var rem_split = reminder.Split("on ");
+                
+                title = rem_split[0];
+                dt = add.Add.FindDate(rem_split[1]);
 
-                C.Success("Successfully added reminder to list.");
-            } catch (Exception e) {
-                C.Error(e.Message);
+                if (dt == DateTime.MinValue) {
+                    C.Error("Invalid Date");
+                    return;
+                }
+                Console.WriteLine(title);
+                Console.WriteLine(dt);
+            } else {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("When: ");
+                Console.ResetColor();
+
+                string? date = Console.ReadLine();
+                if (reminder == null) return;
             }
+
+            // try {
+            //     var con = new NpgsqlConnection(
+            //     connectionString: Program.ConnectionString);
+            //     con.Open();
+            //     using var cmd = new NpgsqlCommand();
+            //     cmd.Connection = con;
+
+            //     // Insert data
+            //     cmd.CommandText = $"INSERT INTO reminders (user_id, title, date) VALUES (@user_id, @title, @date)";
+            //     cmd.Parameters.Add("@user_id", NpgsqlTypes.NpgsqlDbType.Integer).Value = 1;
+            //     cmd.Parameters.Add("@title", NpgsqlTypes.NpgsqlDbType.Varchar).Value = "Reminder from program";
+            //     cmd.Parameters.Add("@date", NpgsqlTypes.NpgsqlDbType.Date).Value = dt;
+            //     await cmd.ExecuteNonQueryAsync();
+
+            //     con.Close();
+
+            //     C.Success("Successfully added reminder to list.");
+            // } catch (Exception e) {
+            //     C.Error(e.Message);
+            // }
         }
     }
 }
