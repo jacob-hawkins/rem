26d7702813350f03867a5403aa72f4305ac2b3a0
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-05-22 17:07:05 -0400

    update sql

--- rem/tables.sql
+++ rem/tables.sql
@@ -1,7 +1,7 @@
 -- DELETE FROM reminders WHERE title = 'test ';
 
 -- DROP TABLE users;
-DROP TABLE work_on;
+-- DROP TABLE work_on;
 DROP TABLE reminders;
 
 CREATE TABLE reminders (
@@ -10,25 +10,28 @@ CREATE TABLE reminders (
 	title VARCHAR(50) NOT NULL,
 	date DATE NOT NULL,
 	completed BOOL DEFAULT False,
-	work_on INT DEFAULT 0
+	work_on_count INT DEFAULT 0,
+	work_on BOOL DEFAULT False,
+	work_on_reminder BIGINT DEFAULT -1
 );
 
-CREATE TABLE work_on (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
-	reminder_id BIGINT REFERENCES reminders(id), -- FORIEGN KEY
-	title VARCHAR(50) NOT NULL,
-	date DATE NOT NULL,
-	completed BOOL DEFAULT False
-);
+-- CREATE TABLE work_on (
+-- 	id BIGSERIAL NOT NULL PRIMARY KEY,
+-- 	reminder_id BIGINT REFERENCES reminders(id), -- FORIEGN KEY
+-- 	title VARCHAR(50) NOT NULL,
+-- 	date DATE NOT NULL,
+-- 	completed BOOL DEFAULT False
+-- );
 
 INSERT INTO reminders (user_id, title, date) VALUES (1, 'test past', '5/14/24');
 INSERT INTO reminders (user_id, title, date) VALUES (1, 'test', '5/16/24');
+INSERT INTO reminders (user_id, title, date, completed) VALUES (1, 'completed', '5/16/24', True);
 
-INSERT INTO work_on (reminder_id, title, date) VALUES (1, 'WORK ON TEST', '5/17/24');
-UPDATE reminders SET work_on=work_on+1 WHERE id = 1;
+INSERT INTO reminders (user_id, title, date, work_on, work_on_reminder) VALUES (1, 'WORK ON TEST', '5/17/24', True, 1);
+UPDATE reminders SET work_on_count=work_on_count+1 WHERE id = 2;
 
-INSERT INTO work_on (reminder_id, title, date) VALUES (1, 'WORK ON TEST 2', '5/18/24');
-UPDATE reminders SET work_on=work_on+1 WHERE id = 1;
+INSERT INTO reminders (user_id, title, date, work_on, work_on_reminder) VALUES (1, 'WORK ON TEST 2', '5/18/24', True, 1);
+UPDATE reminders SET work_on_count=work_on_count+1 WHERE id = 2;
 
 -- SELECT * FROM users;
 SELECT * FROM reminders;
